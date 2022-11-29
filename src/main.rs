18b53d0ff7c8014494866c3f5d9ea9efa8752ddf
bluelisandro <nano0117@gmail.com>
2022-11-29 14:51:16 -0500

    Add bitpack and main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use Rust_Universal_Machine::rumdump::rumdis;
+use Rust_Universal_Machine::rumdump::rumload;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let input = env::args().nth(1);
+    let instructions = rumload::load(input.as_deref());
+    
+    println!("{} instructions", instructions.len());
+
+    for instruction in instructions {
+        println!("{}", rumdis::disassemble(instruction));
+    }
 }
