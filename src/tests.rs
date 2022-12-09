71034c5a2e4f01a2ca3ceac65ae8d175e7385751
benchau <ben842002@uri.edu>
2022-12-08 22:20:46 -0500

    unit test

--- src/tests.rs
+++ src/tests.rs
@@ -1,1 +1,19 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
 
+mod tests {
+    use crate::um::UniversalMachine;
+    use crate::instructions;
+
+    #[test]
+    fn add() {
+        let mut UM = UniversalMachine::new();
+        let val1: u32 = 0b_0000_0001_0000_0000_0000_0000_0000_0001;
+        let val2: u32 = 0b_0000_0010_000000000000000000000011;
+        instructions::load_value(&mut UM, val1);
+        instructions::load_value(&mut UM, val2);
+        instructions::add(&mut UM, 3, 1, 2);
+        assert_eq!(5, UM.r[3]);
+    }
+}
